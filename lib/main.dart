77ef3696def97f1af80c046b97c39260aa051ee1
meriem <meriem@gmail.com>
2024-02-16 16:21:21 +0100

    Add Google Map

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: LoginScreen.idScreen,
+      initialRoute: MainScreen.idScreen,
       routes: {
         RegisterScreen.idScreen: (context) => RegisterScreen(),
         LoginScreen.idScreen: (context) => LoginScreen(),
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   static const String idScreen = "mainScreen";
@@ -9,6 +12,10 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController? newGoogleMapController;
+  static const CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +27,17 @@ class _MainScreenState extends State<MainScreen> {
         ),
         centerTitle: true,
       ),
+      body: Stack(children: [
+        GoogleMap(
+          myLocationButtonEnabled: true,
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controllerGoogleMap.complete(controller);
+            newGoogleMapController = controller;
+          },
+        ),
+      ]),
     );
   }
 }
